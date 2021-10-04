ab88bbc36e948a7021b31334353b2e8477b4246e
bertrandda <bertrandda@users.noreply.github.com>
2021-10-04 22:22:13 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# par-ici-tennis (*Paris i tennis*)
+
+Script to automatically book a tennis court
+
+## Get started
+Create `config.json` file from `config.json.sample` and complete with your preferences.
+
+`location`: a list of courts ordered by preference - [full list](https://tennis.paris.fr/tennis/jsp/site/Portal.jsp?page=tennisParisien&view=les_tennis_parisiens)
+
+`date` a string representing a date formated D/M/YYYY
+
+`hours` a list of hours ordered by preference
+
+`priceType` an array containing price type you can book `Tarif plein` and/or `Tarif réduit`
+
+`courtType` an array containing court type you can book `Découvert` and/or `Couvert`
+
+`players` list of players 3 max (without you)
+
+To pass the payement phase without trouble you need a "carnet de réservation", be carefull you need a "carnet" that maches with your `priceType` & `courtType` [combination](https://tennis.paris.fr/tennis/jsp/site/Portal.jsp?page=rate&view=les_tarifs) chose before
+
+To run this project locally, install the dependencies and run the script:
+
+```sh
+npm install
+npm start
+```
---
+++ index.js
@@ -0,0 +1,104 @@
+const { chromium } = require('playwright')
+const dayjs = require('dayjs')
+const customParseFormat = require('dayjs/plugin/customParseFormat')
+const config = require('./config.json')
+
+dayjs.extend(customParseFormat)
+
+const bookTennis = async () => {
+  const browser = await chromium.launch({ headless: true, slowMo: 10 })
+  const page = await browser.newPage()
+  await page.goto('https://tennis.paris.fr/tennis/jsp/site/Portal.jsp?page=tennis&view=start&full=1')
+
+  const [popup] = await Promise.all([
+    page.waitForEvent('popup'),
+    page.click('#button_suivi_inscription'),
+  ])
+  await popup.waitForLoadState()
+  await popup.fill('#username-login', config.account.email)
+  await popup.fill('#password-login', config.account.password)
+  await popup.click('section >> button')
+
+  await new Promise(r => setTimeout(r, 100));
+
+  for (const location of config.locations) {
+    await page.goto('https://tennis.paris.fr/tennis/jsp/site/Portal.jsp?page=recherche&view=recherche_creneau#!')
+
+    // select tennis location
+    await page.type('.tokens-input-text', location)
+    await new Promise(r => setTimeout(r, 300));
+    await page.press('.tokens-input-text', 'ArrowDown');
+    await page.press('.tokens-input-text', 'Enter');
+
+    // select date
+    await page.click('#when')
+    await new Promise(r => setTimeout(r, 1000));
+    const date = dayjs(config.date, 'D/MM/YYYY')
+    await page.click(`[dateiso="${date.format('DD/MM/YYYY')}"]`)
+    await new Promise(r => setTimeout(r, 300));
+
+    await page.click('#rechercher')
+
+    hoursLoop:
+    for (const hour of config.hours) {
+      const dateDeb = `[datedeb="${date.format('YYYY/MM/DD')} ${hour}:00:00"]`
+      if (await page.$(dateDeb)) {
+        if (await page.isHidden(dateDeb)) {
+          await page.click(`#head${location.replaceAll(' ', '')}${hour}h .panel-title`)
+        }
+
+        const slots = await page.$$(dateDeb)
+        for (const slot of slots) {
+          const bookSlotButton = `[courtid="${await slot.getAttribute('courtid')}"]${dateDeb}`
+          const [priceType, courtType] = await (
+            await (await page.$(`.price-description:left-of(${bookSlotButton})`)).innerHTML()
+          ).split('<br>')
+          if (!config.priceType.includes(priceType) || !config.courtType.includes(courtType)) {
+            continue
+          }
+          await page.click(bookSlotButton)
+          break hoursLoop
+        }
+      }
+    }
+
+    if (await page.title() !== 'Paris | TENNIS - Reservation') {
+      console.log(`Failed to find reservation for ${location}`)
+      continue
+    }
+
+    for (const [i, player] of config.players.entries()) {
+      if (i < config.players.length - 1) {
+        await page.click('.addPlayer')
+      }
+    }
+    await new Promise(r => setTimeout(r, 100));
+
+    for (const [i, player] of config.players.entries()) {
+      await page.fill(`[name="player${i + 1}"] >> nth=0`, player.lastName)
+      await page.fill(`[name="player${i + 1}"] >> nth=1`, player.firstName)
+    }
+
+    await page.keyboard.press('Enter');
+
+    await page.click('[paymentmode="existingTicket"]')
+
+    await page.click('#submit')
+
+    await new Promise(r => setTimeout(r, 100));
+
+    if (await page.$('.confirmReservation')) {
+      console.log(`Réservation faite : ${await (
+        await (await page.$('.address')).textContent()
+      ).trim().replace(/( ){2,}/g, ' ')}`)
+      console.log(`le ${await (
+        await (await page.$('.date')).textContent()
+      ).trim().replace(/( ){2,}/g, ' ')}`)
+      break
+    }
+  }
+
+  await browser.close()
+}
+
+bookTennis()
